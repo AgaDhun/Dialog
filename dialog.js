6f12e890fd391a14d2a5b93c56ddb77e2c926e76
Dhunga <Dhunga@yandex.ru>
2021-09-15 14:57:00 +0300

    Add files via upload

--- dialog.js
+++ dialog.js
@@ -1,104 +1,147 @@
 'use strict';
 
 
-//============Dialog================
+//===--- Dialog ---===
 var dialog = {
-	getDialog:function(){
-	//создать объект для данные("слушатель":"сообщение")
-	const dialogObject = Object.create(null);
-	//запрос файла + загрузка данных файла в объект
-	function getJson(url = "dialog.json")
+    // #url файл .json с текстовым набором подсказок
+	getDialog:function(url = "dialog.json")
 	{
-		//запрос файла .json
-		fetch(url).then(
-			response =>
-			{
-				//проверка на ответ
-				return response.ok ? response.json(): "false";
-			}
-		).then(
-			json =>
-			{
-				//загрузить в данные в объект
-				Object.assign(dialogObject, json);
-			}
-		);
-	};
-	getJson();
-
-	//создать контейнер(<div>) + вставить данные(подсказку,сообщение)
-	function createDialog(target, hint, dialogClass)
-	{
-		let div = document.createElement("div");
-		div.id = "dialogBox";
-		div.className = "dialog_" + dialogClass;
-		div.innerHTML = hint;
-		insertDialog(div, target);
-	}
-	
-	//вставить в документ + css(позиция)
-	function insertDialog(div, target)
-	{	
-		//вставляем 
-		document.body.insertBefore(div,null);
-		//данные(размер + позиция) слушателя
-		let communion = document.getElementById("dialogBox");
-		if (communion) {
-			//данные(размер + позиция) диалога
-			let amountCommunion = communion.getBoundingClientRect();
-			
-			if (target.offsetTop > window.innerHeight/2) {
-				communion.style.cssText = "position:absolute; top:"+((target.offsetTop - (amountCommunion.bottom - amountCommunion.top)) - 5)+"px;left:"+ target.offsetLeft +"px;";
-			}
-			if (target.offsetTop < window.innerHeight/2) {
-				communion.style.cssText = "position:absolute; top:"+((target.offsetTop + target.offsetHeight) + 5)+"px; left:"+target.offsetLeft+"px;";
-			}
-		}
-	}
-	
-	//удалить_диалог
-	function deleteDialog()
-	{
-		let communion = document.getElementById("dialogBox");
-		document.body.removeChild(communion);
-	};
-	
-	//обработчик_события + выбор диалога
-	function eventDialog(event)
-	{
-		this.event = event || window.event;
-		let target = event.target || event.srcElement;
-		for (let key in dialogObject) {
-			if (target.getAttribute("data-dialog") == key) {
-				createDialog(target,dialogObject[key],key);
-			}
-		}
-	}//end eventDialog();
-	
-	//установка слушателя на событие
-	if (Object.keys(dialogObject).length == 0) {
-		//подождать загрузки файла .json
-		setTimeout(function()
+		// объект для данных("слушатель":"сообщение")
+		const dialogObject = Object.create(null);
+		
+		// создать_диалог
+		// создать контейнер(<div>) + вставить данные(подсказку,сообщение)
+		function createDialog(target, data, events)
 		{
-			//обработать объект с данными
-			for(let handler in dialogObject){
-				let listener = document.querySelector("[data-dialog="+handler+"]");
-				//установить слушателя
-				listener.addEventListener("mouseenter",{handleEvent:eventDialog});
-				listener.addEventListener("mouseleave",{handleEvent:deleteDialog});
+            // данные объекта [геометрия]
+            let data_target = target.getBoundingClientRect();
+            // создать контейнер для подсказки
+			let div = document.createElement("div");
+			div.id = "dialogBox";
+			div.innerHTML = data;
+			// вставляем 
+			target.insertBefore(div,null);
+			insertDialog(data_target, events);
+		};
+		// end createDialog()
+		
+		// удалить_диалог
+		function deleteDialog(event)
+		{
+			this.event = event || window.event;
+			let target = event.target || event.srcElement;
+			let prompts = document.getElementById("dialogBox");
+			// удалить
+			target.removeChild(prompts);
+		};
+		// end deleteDialog()
+		
+		// вставить в документ + css(позиция)
+		function insertDialog(data_target, events)
+		{
+			// экран || окно
+            let win_w = window.innerWidth;
+            let win_h = window.innerHeight;
+			// данные(размер + позиция)
+            // событие данные
+            let e_cX = events.clientX;
+            let e_pY = events.pageY;
+            // цель данные
+            let t_t = data_target.top;
+            // подсказка
+			let prompts = document.getElementById("dialogBox");
+            // поверка
+			if (prompts) {
+				// подсказка данные
+				let data_prompts = prompts.getBoundingClientRect();
+                let p_w = data_prompts.width;
+                let p_h = data_prompts.height;
+                let p_left = data_prompts.left;
+                // предварительно размер+позиция
+				let position = "z-index:999;opacity:.9;position:absolute;";
+                // вертикаль сверху
+                if (t_t < win_h/2) {
+                    prompts.style.cssText = position+"top:"+(e_pY+20)+"px;left:"+(e_cX+20)+"px;";
+                    // переполнение слева
+                    if (p_left < 0) {
+                        prompts.style.cssText = position+"top:"+(e_pY+20)+"px;left:"+(win_w/2-p_w)+"px;";
+                    }
+                    // переполнение справа
+                    if (e_cX + p_w > win_w) {
+                        prompts.style.cssText = position+"top:"+(e_pY+20)+"px;left:"+(e_cX-(((e_cX+p_w)-win_w)+20))+"px;";
+                    }
+                }
+                // вертикаль снизу
+                if (t_t > win_h/2) {
+                    prompts.style.cssText = position+"top:"+((e_pY-p_h)-20)+"px;left:"+(e_cX+20)+"px;";
+                    // переполнение слева
+                    if (p_left < 0) {
+                        prompts.style.cssText = position+"top:"+((e_pY-p_h)-20)+"px;left:"+(win_w/2-p_w)+"px;";
+                    }
+                    // переполнение справа
+                    if (e_cX + p_w > win_w) {
+                        prompts.style.cssText = position+"top:"+((e_pY-p_h)-20)+"px;left:"+(e_cX-(((e_cX+p_w)-win_w)+20))+"px;";
+                    }
+                } 
 			}
-		},3000);
-	} else {
-		//обработать объект с данными
-		for(let handler in dialogObject){
-			let listener = document.querySelector("[data-dialog="+handler+"]");
-			//установить слушателя
-			listener.addEventListener("mouseenter",{handleEvent:eventDialog});
-			listener.addEventListener("mouseleave",{handleEvent:deleteDialog});
-		}
-	};
+		};
+		// end insertDialog()
+		
+		// обработчик_события + выбор диалога
+		function eventDialog(event)
+		{
+			let events = event || window.event;
+			let target = events.target || events.srcElement;
+			
+			for (let key in dialogObject) {
+				if (target.getAttribute("data-dialog") == key) {
+					createDialog(target, dialogObject[key], events);
+				}
+			}
+		};
+		// end eventDialog();
+		
+		// запрос файла + загрузка данных из файла в объект
+		// проверка загрузки + установка слушателя
+		function getJson(url)
+		{
+			// запрос файла .json
+			fetch(url).then(
+				response =>
+				{
+					// проверка на ответ
+					return response.ok ? response.json(): "false";
+				}
+			).then(
+				json =>
+				{
+					// загрузить в данные в объект
+					Object.assign(dialogObject, json);
+					
+					// проверка о загружены ли данные
+					if (Object.keys(dialogObject).length) {
+						// обработка данных
+						for (let handler in dialogObject) {
+							let listener = document.querySelector("[data-dialog="+handler+"]");
+							// установить слушателя
+							listener.addEventListener("mouseenter", {handleEvent:eventDialog});
+							listener.addEventListener("mouseleave", {handleEvent:deleteDialog});
+						}
+					}
+				}
+			);
+		};
+		// end getJson();
+		
+		// запуск загрузки данных и установки слушателя на событие
+		getJson(url);
+		// end getJson()
+		
+	}
+	// end_getDialog
+}
+// end_dialog
+/*===--- Dialog-end ---===*/
 
-	}//end_getDialog
-}//end_dialog
-/*===============================Dialog-end==================================*/
+// start
 dialog.getDialog();
